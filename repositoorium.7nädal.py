50567e9edf7adc4a8bb2533acecf16538c46e778
TTaavi <43686722+TarviTiits@users.noreply.github.com>
2018-11-07 18:55:05 +0200

    Update repositoorium.7nädal.py

---
+++ repositoorium.7nädal.py
@@ -0,0 +1,11 @@
+#töötamine paarides, Git, igaüks loob oma repositooriumi
+
+# 1. Uus repo - selleks mine github.com --> uus konto
+# repositoorium on andmebaas, seal on kõik alamkaustad, kõik teie versioonid on salvestatud andmebaasi
+# teine kopeerib oma arvutisse repo klooni, saab lokaalsel arvutil muuuta ja siis psuhiga saab pakkuda oma lokaalse muudetud versiooni reposse
+# alguses lisatud temporary asjana andmebaasi, mida keegi peab üle vaatama
+# alternatiiv on saata paralleelseks versiooniks, mida nimetatakse branch ehk haru
+
+# 2. klooni kohalikku arvutisse, github desktop, git Kraken
+# repo admin vaatab pull request põhjal paralleelselt arendatud haru koodi üle ja kas siis nt asendab master harus asjad või mitte
+# valin millised failid ja siis committ
